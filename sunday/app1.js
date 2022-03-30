acad98ea19e8b8ec4ad017576d04923dc1c14075
14 ghufran almomani <almomanighufran@gmail.com>
2022-03-29 22:29:26 -0600

    javascript

--- sunday/app1.js
+++ sunday/app1.js
@@ -1,67 +1,85 @@
-'use strict'
-let formDiv= document.getElementById("mobForm");
-let thElement=document.getElementById("headTa")
-let tbElement=document.getElementById("boadTa");
-let userfor=[];
-function phone(useName,typeNameD){
-    this.useName=useName;
-    this.typeNameD=typeNameD;
-    this.price=priceMob(50,500);
-    this.cond=conditon();
-      
+'use strict';
+let Butn=document.getElementById("btn");
+let data=[];
 
-    userfor.push(this);
-    renderType();
-    console.log(userfor);
-}
-function priceMob(min,max){
-    return Math.floor(Math.random() * (max - min) + min);
+function storeInLocalStorage(){
+    let stored=JSON.stringify(data);
+    localStorage.setItem('clientsPur',stored);
 }
 
-function conditon() {
-    let price=phone.price;
-    if (price <= 50) {
-        return 'used';
-    } 
-    else {
-        return 'new Device';
-    }
-}
-formDiv.addEventListener("submit",handleSubmit);
- function handleSubmit(event){
-    event.preventDefault();
-    
-    let useName=event.target.useName.value;
-    
-    
-    let typeNameD=event.target.typeNameD.value;
-    
-
-    new phone(useName,typeNameD);
-    console.log(phone);
-
- }
- handleSubmit();
- function renderType() {
-    let trElement = document.createElement("tr");
-    let tdElement1 = document.createElement("td");
-    let tdElement2 = document.createElement("td");
-    let tdElement3 = document.createElement("td");
-    let tdElement4 = document.createElement("td");
-  
-    for (let i = 0; i < userfor.length; i++) {
-      tbElement.appendChild(trElement);
-      trElement.appendChild(tdElement1);
-      trElement.appendChild(tdElement2);
-      trElement.appendChild(tdElement3);
-      trElement.appendChild(tdElement4);
-  
-      tdElement1.textContent = userfor[i].useName;
-      tdElement2.textContent = userfor[i].typeNameD;
-      tdElement3.textContent = userfor[i].price;
-      tdElement4.textContent = userfor[i].cond;
+function CallFromLocalStorage(){
+    let getStored=localStorage.getItem('clientsPur');
+    let StoredData=JSON.parse(getStored);
+    console.log(StoredData)
+    let l=StoredData.length;
+    if (l != 0){
+        for(let i=0; i<l; i++){
+            let newRow= document.createElement('tr');
+            let newCol1= document.createElement('td');
+            let newCol2= document.createElement('td');
+            let newCol3= document.createElement('td');
+            let newCol4= document.createElement('td');
+            newRow.appendChild(newCol1);
+            newRow.appendChild(newCol2);
+            newRow.appendChild(newCol3);
+            newRow.appendChild(newCol4);
+            document.getElementById('boadTa').appendChild(newRow);
+            newCol1.innerHTML=StoredData[i].Name;
+            newCol2.innerHTML=StoredData[i].phoneT;
+            newCol3.innerHTML=StoredData[i].price;
+            newCol4.innerHTML=StoredData[i].cond;
+        }
+        for(let i=l-1; i>=0; i--){
+            data.unshift(StoredData[i]);
+        }
     }
 }
 
+Butn.addEventListener("click", phonePurchase);
+function phonePurchase() {
+    let clientName=document.getElementById('client').value;
+    let phoneType=document.getElementById('typeName').value;
 
+    new Client(clientName,phoneType);
+}
 
+CallFromLocalStorage()
+function Client(clientName,phoneType){
+    this.Name=clientName;
+    this.phoneT=phoneType;
+    this.price= Math.floor(Math.random()*(500-50)+50);
+    let x;
+    if (this.price<100){
+        x= 'Used';
+    }else{
+        x= 'New Device';
+    };
+    this.cond= x;
+    
+
+    console.log(this);
+    data.push(this);
+    renderfun();
+    storeInLocalStorage()
+    console.log(localStorage.getItem('clientsPur'));
+}
+
+function renderfun(){
+    let newRow= document.createElement('tr');
+    let newCol1= document.createElement('td');
+    let newCol2= document.createElement('td');
+    let newCol3= document.createElement('td');
+    let newCol4= document.createElement('td');
+    newRow.appendChild(newCol1);
+    newRow.appendChild(newCol2);
+    newRow.appendChild(newCol3);
+    newRow.appendChild(newCol4);
+    document.getElementById('boadTa').appendChild(newRow);
+
+    let l=data.length-1;
+
+    newCol1.innerHTML=data[l].Name;
+    newCol2.innerHTML=data[l].phoneT;
+    newCol3.innerHTML=data[l].price;
+    newCol4.innerHTML=data[l].cond;
+}
