6f773b15f24680240359af47bfa84779b2bf2c51
14 ghufran almomani <almomanighufran@gmail.com>
2022-04-11 16:52:23 -0600

    javascript

---
+++ wednesday1/app2.js
@@ -0,0 +1,18 @@
+'use strict';
+let result=document.getElementById('result');
+
+function GetDataFromLovalStorage(){
+    let storageData=localStorage.getItem('formData');
+    let parseData=JSON.parse(storageData);
+    console.log(parseData);
+    renderInformation(parseData)
+}
+GetDataFromLovalStorage()
+
+function renderInformation(parseData){
+    parseData.map(function(i){
+        let newSpan=document.createElement('span');
+        result.appendChild(newSpan);
+        newSpan.innerHTML= `Hello! This is ${i.fullName}, I was born on ${i.bdate} and my email is: ${i.femail}`;
+    })
+}
